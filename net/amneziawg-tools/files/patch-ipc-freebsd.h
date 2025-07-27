d9648eeed03a37c7ccb3a99a4b1e5356191f1ffb
Vladimir Grebenshchikov <vova@zote.me>
2025-07-27 23:22:36 +0200

    Update wireguard-tools to the FreeBSD-base latest version - 1.0.20250521, which has some stubbed out support for incremental Allowed-IPs updates that we would need to add kernel support for.
      (cherry picked from commit 137de4b34d45192985e21f6d6163533da547fbac)

--- net/amneziawg-tools/files/patch-ipc-freebsd.h
+++ net/amneziawg-tools/files/patch-ipc-freebsd.h
@@ -1,11 +1,20 @@
---- ipc-freebsd.h.orig	2025-07-22 19:01:39 UTC
+--- ipc-freebsd.h.orig	2024-10-01 13:02:42 UTC
 +++ ipc-freebsd.h
-@@ -21,7 +21,7 @@ static int kernel_get_wireguard_interfaces(struct stri
- 
- static int kernel_get_wireguard_interfaces(struct string_list *list)
+@@ -15,7 +15,7 @@ static int get_dgram_socket(void)
  {
--	struct ifgroupreq ifgr = { .ifgr_name = "wg" };
-+	struct ifgroupreq ifgr = { .ifgr_name = "awg" };
- 	struct ifg_req *ifg;
- 	int s = get_dgram_socket(), ret = 0;
+ 	static int sock = -1;
+ 	if (sock < 0)
+-		sock = socket(AF_INET, SOCK_DGRAM, 0);
++		sock = socket(AF_LOCAL, SOCK_DGRAM, 0);
+ 	return sock;
+ }
  
+@@ -389,6 +389,8 @@ static int kernel_set_device(struct wgdevice *dev)
+ 			nvl_aips[j] = nvlist_create(0);
+ 			if (!nvl_aips[j])
+ 				goto err_peer;
++			if (aip->flags)
++				nvlist_add_number(nvl_aips[j], "flags", aip->flags);
+ 			nvlist_add_number(nvl_aips[j], "cidr", aip->cidr);
+ 			if (aip->family == AF_INET)
+ 				nvlist_add_binary(nvl_aips[j], "ipv4", &aip->ip4, sizeof(aip->ip4));
